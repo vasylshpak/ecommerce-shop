e3fa345128d04db05d447bf67d6b4683dd5b035f
Vasyl Shpak <vasyl.shpak4@gmail.com>
2021-02-17 19:52:18 +0100

    add: product component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-        <Row>
+        <Row>w
             {products.map((product)=>(
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product} />
