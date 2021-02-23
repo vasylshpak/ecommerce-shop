f0185d0345df238c606d42c810afcd31ad5bbaff
Vasyl Shpak <vasyl.shpak4@gmail.com>
2021-02-23 20:39:53 +0100

    add:server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-        <Row>w
+        <Row>
             {products.map((product)=>(
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product} />
