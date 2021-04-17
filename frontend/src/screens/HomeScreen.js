5f7c313bd522f99708960da8383ce2cfbc0dc56f
Vasyl Shpak <vasyl.shpak4@gmail.com>
2021-04-17 21:57:40 +0200

    implement redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
 import Product from '../components/Product'
+import { useDispatch, useSelector} from "react-redux";
 import {Row, Col} from 'react-bootstrap'
+import {listProducts} from "../actions/productAction";
+import {load} from "dotenv";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
 
-  const [products, setProducts] = useState([])
+  const productList = useSelector(state => state.productList)
+  const {loading, products, error} = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
+  
   return (
     <>
       <h1>Latest Products</h1>
-        <Row>
+      {loading
+          ? <h2>Loading...</h2>
+          : error ? <h3>{error}</h3>
+          : <Row>
             {products.map((product)=>(
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
+                  <Product product={product} />
                 </Col>
             ))}
-        </Row>
+          </Row>}
+
     </>
   )
 }
